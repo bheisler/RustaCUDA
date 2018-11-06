f47015fc5da2f06e5e1337ff8540067b080c3680
Brook Heisler <redattack34@gmail.com>
2018-11-05 20:05:26 -0600

    Add basic driver initialization and versioning functions.

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@
 // Allow clippy lints
 #![allow(unknown_lints)]
 
+#[macro_use]
+extern crate bitflags;
 extern crate cuda_sys;
 
 pub mod error;
@@ -17,3 +19,70 @@ pub mod memory;
 pub(crate) mod private;
 
 mod derive_compile_fail;
+
+use cuda_sys::cuda::{cuDriverGetVersion, cuInit};
+use error::{CudaResult, ToResult};
+
+/*
+TODO:
+- Implement device enumeration, context management, basic module management
+- It may be useful to have a quick-init function that initialized the driver and binds a context to
+  the first device found. That's what most people will want, and if it's a well-documented helper
+  method that specifies what it actually does, it's probably fine.
+*/
+
+bitflags! {
+    /// Flags for initializing the CUDA driver. Currently, no flags are defined,
+    /// so ZERO is the only valid value.
+    pub struct CudaFlags: u32 {
+        /// No flags set. As there are currently no flags defined, this is the only accepted value.
+        const ZERO = 0;
+    }
+}
+
+/// Initialize the CUDA Driver API. This must be called before any other RustaCUDA (or CUDA) function
+/// is called. Typically, this should be at the start of your program. All other functions will fail
+/// unless the API is initialized first.
+pub fn init(flags: CudaFlags) -> CudaResult<()> {
+    unsafe { cuInit(flags.bits()).toResult() }
+}
+
+/// Struct representing the CUDA API version number.
+#[derive(Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Copy, Clone)]
+pub struct CudaApiVersion {
+    version: i32,
+}
+impl CudaApiVersion {
+    /// Returns the latest CUDA version supported by the CUDA driver.
+    pub fn get() -> CudaResult<CudaApiVersion> {
+        unsafe {
+            let mut version: i32 = 0;
+            cuDriverGetVersion(&mut version as *mut i32).toResult()?;
+            Ok(CudaApiVersion { version })
+        }
+    }
+
+    /// Return the major version number - eg. the 9 in version 9.2
+    #[inline]
+    pub fn major(self) -> i32 {
+        self.version / 1000
+    }
+
+    /// Return the minor version number - eg. the 2 in version 9.2
+    #[inline]
+    pub fn minor(self) -> i32 {
+        (self.version % 1000) / 10
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_api_version() {
+        let version = CudaApiVersion { version: 9020 };
+        assert_eq!(version.major(), 9);
+        assert_eq!(version.minor(), 2);
+    }
+}
